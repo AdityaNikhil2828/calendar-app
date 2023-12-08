1a7d7eeb617755eacb8a7fa546cf2ef43cadfc8c
Aaron Vincent 6411 <aaronvincent6411@gmail.com>
2023-12-08 13:51:28 +0530

    made some changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,25 @@
 import 'package:calendar_app/services/themeservice.dart';
+import 'package:calendar_app/ui/authui.dart';
+import 'package:calendar_app/ui/home.dart';
 import 'package:calendar_app/ui/theme.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get_navigation/src/root/get_material_app.dart';
 import 'package:get_storage/get_storage.dart';
-import 'ui/home.dart';
+import 'package:firebase_core/firebase_core.dart';
+import './firebase_options.dart';
+// import 'ui/home.dart';
+// import 'package:calendar_app/ui/calendar_page.dart';
+
 
 Future <void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await GetStorage.init();
+   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(MyApp());
 }
+
+// options: DefaultFirebaseOptions.currentPlatform
 
 class MyApp extends StatelessWidget {
 
@@ -21,7 +31,11 @@ class MyApp extends StatelessWidget {
       theme: Themes.light,
       darkTheme: Themes.dark,
       themeMode: ThemeServices().theme,
-      home: HomePage()
+      initialRoute: '/', 
+      routes: {
+        '/': (context) => HomePage(),
+        '/sign_in': (context) => SignInPage(), 
+      },
     );
   }
 }
